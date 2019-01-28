51cce38f4c2867e7bfb87cac806f0443c9feb015
MehediHasanSunBSSE1025 <46520339+MehediHasanSunBSSE1025@users.noreply.github.com>
2019-01-29 00:48:16 +0600

    Add files via upload

--- Copying_image.c
+++ Copying_image.c
@@ -1,10 +1,30 @@
+//it can copy an image
+
+
 #include<stdio.h>
 int main ()
 {
 	FILE *imgInput, *imgOutput;
 	int i;
-	imgInput = fopen("lena_gray.bmp", "rb"); 
-	imgOutput = fopen("copy_of_lena_gray.bmp", "wb"); 
+	char name[100];
+	scanf("%s",name);
+	
+	imgInput = fopen(name, "rb"); 
+	
+	int len=strlen(name);
+	name[--len]='Y';
+	name[--len]='P';
+	name[--len]='O';
+	name[--len]='C';
+	
+	len=strlen(name);
+	name[len++]='.';
+	name[len++]='b';
+	name[len++]='m';
+	name[len++]='p';
+	
+	
+	imgOutput = fopen(name, "wb"); 
 
 	char ch;
 	while (!feof(imgInput))
